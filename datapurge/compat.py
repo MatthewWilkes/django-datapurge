67811e43ccb1e14042e2576cf7f953497f5f3df1
Matthew Wilkes <git@matthewwilkes.name>
2016-04-06 10:27:25 +0100

    Django 1.8 and 1.9 support

--- datapurge/compat.py
+++ datapurge/compat.py
@@ -2,7 +2,10 @@
 from functools import partial
 import django
 
-if django.VERSION >= (1,7):
+if django.VERSION >= (1,9):
+    from django.apps import apps
+    get_model = partial(apps.get_model)
+elif django.VERSION >= (1,7):
     from django.db.models import get_model
     get_model = partial(get_model)
 elif django.VERSION >= (1,4):
