4c347a1df1a353332e979316451b4ef8059031c6
Matthew Wilkes <git@matthewwilkes.name>
2016-04-06 10:19:53 +0100

    Django 1.7 compatibility

--- datapurge/compat.py
+++ datapurge/compat.py
@@ -2,7 +2,10 @@
 from functools import partial
 import django
 
-if django.VERSION >= (1,4):
+if django.VERSION >= (1,7):
+    from django.db.models import get_model
+    get_model = partial(get_model)
+elif django.VERSION >= (1,4):
     from django.db.models import get_model
     get_model = partial(get_model, seed_cache=False, only_installed=True)
 else:
